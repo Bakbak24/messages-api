d12f1a0ac7bf0560b59eaf92ed04296b5e177f33
Bakbak24 <zakabak3@gmail.com>
2024-10-04 14:52:56 +0200

    Refactor: Add CORS and update routes in app.js and messages.js

--- app.js
+++ app.js
@@ -20,9 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 // CORS is used to allow the API to be accessed from any domain.
+app.use(cors());
+
 app.use('/', indexRouter); // This is the route that will be used to get the data from the API
 app.use('/users', usersRouter); // This is the route that will be used to get the data from the API
 app.use('/api/v1/messages', require('./routes/api/v1/messages')); // This is the route that will be used to get the data from the API
--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -2,24 +2,75 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.json({
+  res.status(200).json({
     status: "success",
     message: "GET messages",
     data: {
-        messages: [
-            {
-                user: "John",
-                message: "Hello World"
-            },
-            {
-                user: "Maria",
-                message: "Hello Universe"
-            }
-        ]
+      messages: [
+        {
+          user: "John",
+          message: "Hello World",
+        },
+        {
+          user: "Maria",
+          message: "Hello Universe",
+        },
+      ],
     },
   });
 });
 
+router.get("/:id", (req, res, next) => {
+  const messageId = parseInt(req.params.id, 10);
+  const messages = [
+    {
+      user: "John",
+      message: "Hello World",
+    },
+    {
+      user: "Maria",
+      message: "Hello Universe",
+    },
+  ];
 
+  if (messageId > 0 && messageId <= messages.length) {
+    res.status(200).json({
+      status: "success",
+      message: `GETTING message with ID ${messageId}`,
+      data: {
+        message: messages[messageId - 1],
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      message: "Message not found",
+    });
+  }
+});
+
+router.post("/", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "POST message",
+    data: {
+      message: {
+        user: req.body.user,
+        message: req.body.message,
+      },
+    },
+  });
+});
+
+//  PUT: /api/v1/messages/:id
+router.put("/:id", (req, res, next) => {
+  const messageId = parseInt(req.params.id, 10);
+  const messages = [
+    {
+      user: "John",
+      message: "Hello World",
+    },
+  ];
+});
 
 module.exports = router;
